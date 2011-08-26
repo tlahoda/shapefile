a03ae410846dd14fd0407f023a1109a24a296a96
Thomas Lahoda <tlahoda@gmail.com>
2011-08-25 21:31:41 -0400

    Removed orthoPoint as it is not needed.

--- shapefile_render.js
+++ shapefile_render.js
@@ -19,16 +19,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-function orthoPoint (x, y) {
-  this.x = x;
-  this.y = y;
-}
 
 function ll2Orthoxy (lat, lon) {
   lat *= deg2rad;
   lon *= deg2rad;
   var slat = Math.sin (piOverTwo - lat);
-  return new orthoPoint (400 + slat * Math.cos (lon) * 800, 800 - (800 + slat * Math.sin (lon) * 800));
+  return new Point (400 + slat * Math.cos (lon) * 800, 800 - (800 + slat * Math.sin (lon) * 800));
 }
 
 function render (shapeFile, context, color) {
