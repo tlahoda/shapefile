85ab52580ca50ab0bfd3ba17264f16f48dd5a5af
Thomas Lahoda <tlahoda@gmail.com>
2011-08-27 14:07:58 -0400

    Moved shape transform function to members of their shape class.

--- shapefile.js
+++ shapefile.js
@@ -26,7 +26,7 @@ function load_binary_resource (url) {
   req.overrideMimeType('text/plain; charset=x-user-defined');
   req.send (null);
   //Change 200 to 0 for local file testing
-  if (req.status != 0) throw "Unable to load " + url;
+  if (req.status != 200) throw "Unable to load " + url;
     return req.responseText;
 }
 
@@ -54,15 +54,20 @@ function Header (shx) {
 }
 
 /**
- * Represents a null shape.
+ * The base shape class. Also represents a null shape.
  *
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function NullShape (shapeType, shp) {
-  this.header = new Array (1);
-  this.header[0] = shapeType;
-}
+var Shape = Class.create ({
+  initialize: function (shapeType, shp) {
+    this.header = new Array (1);
+    this.header[0] = shapeType;
+  },
+  transform: function (transformFunction) {
+    //Does nothing.
+  }
+});
 
 /**
  * Represents a point.
@@ -70,65 +75,17 @@ function NullShape (shapeType, shp) {
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function Point (shapeType, shp) {
-  this.header = new Array (1);
-  this.header[0] = shapeType;
-  var x = shp.readDouble ();
-  var y = shp.readDouble ();
-  this.coords = [x, y];
-}
-
-/**
- * Represents a polygon.
- *
- * \param shapeType The type of the shape.
- * \param shp The binaryReader containing the main shapefile.
- */
-function Polygon (shapeType, shp) {
-  this.header = new Array (6);
-  this.header[0] = shapeType;
-  for (var i = 1; i < 5; ++i)
-    this.header[i] = shp.readDouble ();
-  for (var i = 5; i < 7; ++i)
-    this.header[i] = shp.readInt32 ();
-
-  var partsIndex = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i)
-    partsIndex[i] = shp.readInt32 ();
-
-  this.parts = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i) {
-    var length = ((i == this.header[5] - 1) ? this.header[6] : partsIndex[i + 1]) - partsIndex[i];
-    this.parts[i] = new Array (length);
-
-    for (var j = 0; j < length; ++j) {
-      var lon = shp.readDouble ();
-      var lat = shp.readDouble ();
-      this.parts[i][j] = [lat, lon];
-    }
-  }
-}
-
-/**
- * Represents a set of points.
- *
- * \param shapeType The type of the shape.
- * \param shp The binaryReader containing the main shapefile.
- */
-function MultiPoint (shapeType, shp) {
-  this.header = new Array (6);
-  this.header[0] = shapeType;
-  for (var i = 1; i < 5; ++i)
-    this.header[i] = shp.readDouble ();
-  this.header[5] = shp.readInt32 ();
-
-  this.points = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i) {
+var Point = Class.create (Shape, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
     var x = shp.readDouble ();
     var y = shp.readDouble ();
-    this.points[i] = [x, y];
+    this.coords = [0, 0];//[x, y];
+  },
+  transform: function (transformFunction) {
+    this.coords = transformFunction (shape.coords);
   }
-}
+});
 
 /**
  * Represents a PointZ.
@@ -136,39 +93,15 @@ function MultiPoint (shapeType, shp) {
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function PointZ (shapeType, shp) {
-  throw "PointZ not implemented.";
-}
-
-/**
- * Represents a PolyLineZ.
- *
- * \param shapeType The type of the shape.
- * \param shp The binaryReader containing the main shapefile.
- */
-function PolyLineZ (shapeType, shp) {
-  throw "PolyLineZ not implemented.";
-}
-
-/**
- * Represents a PolygonZ.
- *
- * \param shapeType The type of the shape.
- * \param shp The binaryReader containing the main shapefile.
- */
-function PolygonZ (shapeType, shp) {
-  throw "PolygonZ not implemented.";
-}
-
-/**
- * Represents a set of MultiPointZs.
- *
- * \param shapeType The type of the shape.
- * \param shp The binaryReader containing the main shapefile.
- */
-function MultiPointZ (shapeType, shp) {
-  throw "MultiPointZ not implemented.";
-}
+var PointZ = Class.create (Point, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    throw "PointZ not implemented.";
+  },
+  transform: function (transformFunction) {
+    throw "PointZ transforming not implemented.";
+  }
+});
 
 /**
  * Represents a measured point.
@@ -176,56 +109,57 @@ function MultiPointZ (shapeType, shp) {
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function PointM (shapeType, shp) {
-  this.header = new Array (1);
-  this.header[0] = shapeType;
-  var x = shp.readDouble ();
-  var y = shp.readDouble ();
-  this.coords = [x, y];
-  this.m = shp.readDouble ();
-}
+var PointM = Class.create (Point, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    this.m = shp.readDouble ();
+  },
+  transform: function (transformFunction) {
+    this.coords = transformFunction (shape.coords);
+  }
+});
 
 /**
- * Represents a PolygonM.
+ * Represents a set of points.
  *
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function PolygonM (shapeType, shp) {
-  this.header = new Array (6);
-  this.header[0] = shapeType;
-  for (var i = 1; i < 5; ++i)
-    this.header[i] = shp.readDouble ();
-  for (var i = 5; i < 7; ++i)
-    this.header[i] = shp.readInt32 ();
+var MultiPoint = Class.create (Shape, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    for (var i = 1; i < 5; ++i)
+      this.header[i] = shp.readDouble ();
+    this.header[5] = shp.readInt32 ();
 
-  var partsIndex = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i)
-    partsIndex[i] = shp.readInt32 ();
-
-  this.parts = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i) {
-    var length = ((i == this.header[5] - 1) ? this.header[6] : partsIndex[i + 1]) - partsIndex[i];
-    this.parts[i] = new Array (length);
-
-    for (var j = 0; j < length; ++j) {
-      var lon = shp.readDouble ();
-      var lat = shp.readDouble ();
-      this.parts[i][j] = [lat, lon];
+    this.points = new Array (this.header[5]);
+    for (var i = 0; i < this.header[5]; ++i) {
+      var x = shp.readDouble ();
+      var y = shp.readDouble ();
+      this.points[i] = [x, y];
     }
+  },
+  transform: function (transformFunction) {
+    for (var i = 0; i < this.header[5]; ++i)
+      this.points[i] = transformFunction (this.points[i]);
   }
+});
 
-  this.Mmin = shp.readDouble ();
-  this.Mmax = shp.readDouble ();
-  this.Mparts = new Array ();
-  for (var i = 0; i < this.header[5]; ++i) {
-    var length = ((i == this.header[5] - 1) ? this.header[6] : partsIndex[i + 1]) - partsIndex[i];
-    this.Mparts[i] = new Array (length);
-
-    for (var j = 0; j < length; ++j)
-      this.Mparts[i][j] = shp.readDouble ();
+/**
+ * Represents a set of MultiPointZs.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var MultiPointZ = Class.create (MultiPoint, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    throw "MultiPointZ not implemented.";
+  },
+  transform: function (transformFunction) {
+    throw "MultiPointZ transforming not implemented.";
   }
-}
+});
 
 /**
  * Represents a set of measured points.
@@ -233,26 +167,151 @@ function PolygonM (shapeType, shp) {
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function MultiPointM (shapeType, shp) {
-  this.header = new Array (6);
-  this.header[0] = shapeType;
-  for (var i = 1; i < 5; ++i)
-    this.header[i] = shp.readDouble ();
-  this.header[5] = shp.readInt32 ();
-
-  this.points = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i) {
-    var x = shp.readDouble ();
-    var y = shp.readDouble ();
-    this.points[i] = [x, y];
+var MultiPointM = Class.create (MultiPoint, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    this.Mmin = shp.readDouble ();
+    this.Mmax = shp.readDouble ();
+    this.Marray = new Array (this.header[5]);
+    for (var i = 0; i < this.header[5]; ++i)
+      this.Marray[i] = shp.readDouble ();
+  },
+  transform: function (transformFunction) {
+    for (var i = 0; i < this.header[5]; ++i)
+      this.points[i] = transformFunction (this.points[i]);
   }
+});
 
-  this.Mmin = shp.readDouble ();
-  this.Mmax = shp.readDouble ();
-  this.Marray = new Array (this.header[5]);
-  for (var i = 0; i < this.header[5]; ++i)
-    this.Marray[i] = shp.readDouble ();
-}
+/**
+ * Represents a polygon.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var Polygon = Class.create (Shape, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    for (var i = 1; i < 5; ++i)
+      this.header[i] = shp.readDouble ();
+    for (var i = 5; i < 7; ++i)
+      this.header[i] = shp.readInt32 ();
+
+    var partsIndex = new Array (this.header[5]);
+    for (var i = 0; i < this.header[5]; ++i)
+      partsIndex[i] = shp.readInt32 ();
+
+    this.parts = new Array (this.header[5]);
+    for (var i = 0; i < this.header[5]; ++i) {
+      var length = ((i == this.header[5] - 1) ? this.header[6] : partsIndex[i + 1]) - partsIndex[i];
+      this.parts[i] = new Array (length);
+
+      for (var j = 0; j < length; ++j) {
+        var lon = shp.readDouble ();
+        var lat = shp.readDouble ();
+        this.parts[i][j] = [lat, lon];
+      }
+    }
+  },
+  transform: function (transformFunction) {
+    for (var i = 0; i < this.header[5]; ++i)
+      for (var j = 0; j < this.parts[i].length; ++j)
+        this.parts[i][j] = transformFunction (this.parts[i][j]);
+  }
+});
+
+/**
+ * Represents a PolygonZ.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var PolygonZ = Class.create (Polygon, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    throw "PolygonZ not implemented.";
+  },
+  transform: function (transformFunction) {
+    throw "PolygonZ transforming not implemented.";
+  }
+});
+
+/**
+ * Represents a PolygonM.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var PolygonM = Class.create (Polygon, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    this.Mmin = shp.readDouble ();
+    this.Mmax = shp.readDouble ();
+    this.Mparts = new Array ();
+    for (var i = 0; i < this.header[5]; ++i) {
+      var length = ((i == this.header[5] - 1) ? this.header[6] : partsIndex[i + 1]) - partsIndex[i];
+      this.Mparts[i] = new Array (length);
+
+      for (var j = 0; j < length; ++j)
+        this.Mparts[i][j] = shp.readDouble ();
+    }
+  },
+  transform: function (transformFunction) {
+    for (var i = 0; i < this.header[5]; ++i)
+      for (var j = 0; j < this.parts[i].length; ++j)
+        this.parts[i][j] = transformFunction (this.parts[i][j]);
+  }
+});
+
+/**
+ * Represents a PolyLine.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var PolyLine = Class.create (Polygon, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+  },
+  transform: function (transformFunction) {
+    for (var i = 0; i < this.header[5]; ++i)
+      for (var j = 0; j < this.parts[i].length; ++j)
+        this.parts[i][j] = transformFunction (this.parts[i][j]);
+  }
+});
+
+/**
+ * Represents a PolyLineZ.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var PolyLineZ = Class.create (PolygonZ, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    throw "PolyLineZ not implemented.";
+  },
+  transform: function (transformFunction) {
+    throw "PolyLineZ transforming not implemented.";
+  }
+});
+
+/**
+ * Represents a PolyLineM.
+ *
+ * \param shapeType The type of the shape.
+ * \param shp The binaryReader containing the main shapefile.
+ */
+var PolyLineM = Class.create (PolygonM, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    throw "PolyLineM not implemented.";
+  },
+  transform: function (transformFunction) {
+    for (var i = 0; i < this.header[5]; ++i)
+      for (var j = 0; j < this.parts[i].length; ++j)
+        this.parts[i][j] = transformFunction (this.parts[i][j]);
+  }
+});
 
 /**
  * Represents a MultiPatch.
@@ -260,9 +319,15 @@ function MultiPointM (shapeType, shp) {
  * \param shapeType The type of the shape.
  * \param shp The binaryReader containing the main shapefile.
  */
-function MultiPatch (shapeType, shp) {
-  throw "MultiPatch not implemented.";
-}
+var MultiPatch = Class.create (Shape, {
+  initialize: function ($super, shapeType, shp) {
+    $super (shapeType, shp);
+    throw "MultiPatch not implemented.";
+  },
+  transform: function (transformFunction) {
+    throw "MultiPatch transforming not implemented.";
+  }
+});
 
 /**
  * Creates the appropriate shape type.
@@ -273,14 +338,13 @@ function MultiPatch (shapeType, shp) {
 function ShapeFactory (shapeType, shp) {
   switch (shapeType) {
     case 0:
-      return new NullShape (shapeType, shp);
+      return new Shape (shapeType, shp);
       break;
     case 1:
       return new Point (shapeType, shp);
       break;
     case 3:
-      //Polyline has the same structure as Polygon so using it for now.
-      return new Polygon (shapeType, shp);
+      return new PolyLine (shapeType, shp);
       break;
     case 5:
       return new Polygon (shapeType, shp);
@@ -304,8 +368,7 @@ function ShapeFactory (shapeType, shp) {
       return new PointM (shapeType, shp);
       break;
     case 23:
-      //PolylineM has the same structure as PolygonM so using it for now.
-      return new PolygonM (shapeType, shp);
+      return new PolyLineM (shapeType, shp);
       break;
     case 25:
       return new PolygonM (shapeType, shp);
