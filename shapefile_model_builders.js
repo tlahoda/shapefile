c22a69142e01d1f41eca065dfcf3c20d8bdceaca
Thomas Lahoda <tlahoda@gmail.com>
2011-08-29 15:47:55 -0400

    Refactored to use the new functional programming facilities.

--- shapefile_model_builders.js
+++ shapefile_model_builders.js
@@ -20,257 +20,126 @@
  */
 
 /**
- * Renders a point in a 3d.
+ * Build the model for a point.
  *
  * \param shape The shape to render.
- * \param context The context onto which to render.
+ * \param color The color to render the shape.
  */
-var PointBuilder = Class.create ({
-  initialize: function (shape, color) {
-    var data = [["vertex", shape.coords], 
-                ["color", color],
-                ["wglu_elements", [0, 1, 2, 0, 3, 4, 0, 3, 1, 0, 2, 4]]];
+function buildPointModel (shape, color) {
+  var data = [["vertex", shape.coords], 
+              ["color", color],
+              ["wglu_elements", [0, 1, 2, 0, 3, 4, 0, 3, 1, 0, 2, 4]]];
 
-    shape.model = $W.createObject({ type: $W.GL.POINTS, data: data });
-    shape.model.setPosition (0, 0, 0);
-    shape.model.animation._update = function() { this.setRotation(this.age / 60, 0, 0); }
-  }
-});
-
-/**
- * Renders a PointZ point in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PointZBuilder = Class.create (PointBuilder, {
-  initialize: function ($super, shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a measured point in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PointMBuilder = Class.create (PointBuilder, {
-  initialize: function ($super, shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a set of multiple points in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var MultiPointBuilder = Class.create ({
-  initialize: function (shape, color) {
-    var colors = new Array ();
-    for (var i = 0; i < shape.points.length; ++i)
-      colors[i] = color;
-  
-    var data = [["vertex", shape.points], 
-                ["color", colors],
-                ["wglu_elements", [0, 1, 2, 0, 3, 4, 0, 3, 1, 0, 2, 4]]];
-
-    shape.model = $W.createObject({ type: $W.GL.POINTS, data: data });
-    shape.model.setPosition (0, 0, 0);
-    shape.model.animation._update = function() { this.setRotation(this.age / 60, 0, 0); }
-  }
-});
-
-/**
- * Renders a set of PointZs in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var MultiPointZBuilder = Class.create (MultiPointBuilder, {
-  initialize: function ($super, shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a set of measured points in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var MultiPointMBuilder = Class.create (MultiPointBuilder, {
-  initialize: function ($super, shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a polygon in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonBuilder = Class.create ({
-  initialize: function (shape, color) {
-    shape.models = new Array (shape.parts.length);
-
-    for (var j = 0; j < shape.parts.length; ++j) {
-      var part = shape.parts[j];
-
-      var colors = new Array ();
-      for (var k = 0; k < part.length; ++k)
-        colors[k] = color;
-
-      var data = [["vertex", part], 
-                  ["color", colors],
-                  ["wglu_elements", [0, 1, 2, 0, 3, 4, 0, 3, 1, 0, 2, 4]]];
-
-      shape.models[j] = $W.createObject({ type: $W.GL.LINE_LOOP, data: data });
-      shape.models[j].setPosition (0, 1, 0);
-      shape.models[j].animation._update = function() { this.setRotation(this.age / 60, 0, 0);}
-    }
-  }
-});
-
-/**
- * Renders a PolygonZ in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonZBuilder = Class.create (PolygonBuilder, {
-  initialize: function ($super, shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a measured polygon in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonMBuilder = Class.create (PolygonBuilder, {
-  initialize: function (shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a PolyLine in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolyLineBuilder = Class.create (PolygonBuilder, {
-  initialize: function(shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a PolyLineZ in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolyLineZBuilder = Class.create (PolygonBuilder, {
-  initialize: function(shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a PolyLineM in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolyLineMBuilder = Class.create (PolygonBuilder, {
-  initialize: function(shape, color) {
-    $super (shape, color);
-  }
-});
-
-/**
- * Renders a MultiPatch in a 3d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var MultiPatchBuilder = Class.create ({
-  initialize: function (shape, color) {
-    throw "MultiPatch rendering not implemented.";
-  }
-});
-
-/**
- * Calls the appropriate renderer.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-function ModelBuilderFactory (shape, color) {
-  switch (shape.header[0]) {
-    case 0:
-      //NullShape does not need rendering.
-      break;
-    case 1:
-      new PointBuilder (shape, color);
-      break;
-    case 3:
-      new PolyLineBuilder (shape, color);
-      break;
-    case 5:
-      new PolygonBuilder (shape, color);
-      break;
-    case 8:
-      new MultiPointBuilder (shape, color);
-      break;
-    case 11:
-      new PointZBuilder (shape, color);
-      break;
-    case 13:
-      new PolyLineZBuilder (shape, color);
-      break;
-    case 15:
-      new PolygonZBuilder (shape, color);
-      break;
-    case 18:
-      new MultiPopintZBuilder (shape, color);
-      break;
-    case 21:
-      new PointMBuilder (shape, color);
-      break;
-    case 23:
-      new PolyLineMBuilder (shape, color);
-      break;
-    case 25:
-      new PolygonMBuilder (shape, color);
-      break;
-    case 28:
-      new MultiPointMBuilder (shape, color);
-      break;
-    case 31:
-      new MultiPatchBuilder (shape, color);
-      break;
-    default:
-      throw "Shape type unknown.";
-  }
+  shape.model = $W.createObject({ type: $W.GL.POINTS, data: data });
+  shape.model.setPosition (0, 0, 0);
+  shape.model.animation._update = function() { this.setRotation(this.age / 60, 0, 0); }
 }
 
 /**
- * Renders a shapefile in 3d.
+ * Builds the model for a MultiPoint.
  *
- * \param shapeFile The shapefile.
- * \param context The canvas onto which to draw.
- * \param color, The color to render the shapes.
+ * \param shape The shape to render.
+ * \param color The color to render the shape.
  */
-function buildModels (shapeFile, color) {
-  //for (var i = 0; i < shapeFile.header.numShapes; ++i)
-    ModelBuilderFactory (shapeFile.shapes[27], color);
+function buildMultiPointModel (shape, color) {
+  var colors = new Array ();
+  for (var i = 0; i < shape.points.length; ++i)
+    colors[i] = color;
+
+  var data = [["vertex", shape.points], 
+              ["color", colors],
+              ["wglu_elements", [0, 1, 2, 0, 3, 4, 0, 3, 1, 0, 2, 4]]];
+
+  shape.model = $W.createObject({ type: $W.GL.POINTS, data: data });
+  shape.model.setPosition (0, 0, 0);
+  shape.model.animation._update = function() { this.setRotation(this.age / 60, 0, 0); }
+}
+
+/**
+ * Colors a part.
+ *
+ * \param part The part to color.
+ * \param color The color to make the part.
+ */
+function makePolygonPartColors (part, color) {
+  var colors = new Array ();
+  for (var k = 0; k < part.length; ++k)
+    colors[k] = color;
+  return colors;
+}
+
+/**
+ * Builds the models for a polygon part.
+ *
+ * \param part The part for which to build the models.
+ * \param color The color of the part.
+ */
+function buildPolygonPartModel (part, color) {
+  var data = [["vertex", part], 
+              ["color", makePolygonPartColors (part, color)],
+              ["wglu_elements", [0, 1, 2, 0, 3, 4, 0, 3, 1, 0, 2, 4]]];
+
+  part.model = $W.createObject({ type: $W.GL.POINTS, data: data });
+  part.model.setPosition (0, 1, 0);
+  part.model.animation._update = function() { this.setRotation(this.age / 60, 0, 0);}
+  return part;
+}
+
+/**
+ * Builds the models for a Polygon.
+ *
+ * \param shape The shape to render.
+ * \param color The color to render the shape.
+ */
+function buildPolygonModels (shape, color) {
+  shape.eachPart (buildPolygonPartModel, color);
+}
+
+/**
+ * Builds the models for a MultiPatch.
+ *
+ * \param shape The shape to render.
+ * \param color The color to render the shape.
+ */
+function buildMultiPatchModels (shape, color) {
+  //not yet implemented.
+}
+
+/**
+ * Builds the modesl for a shape.
+ *
+ * \param shape The shape to render.
+ * \param color The color to render the shape.
+ */
+function buildModels (shape, color) {
+  switch (shape.header[0]) {
+    case 0:
+      //NullShape does not need a model.
+      break;
+    case 1:
+    case 11: //PointZ
+    case 21: //PointM
+      buildPointModel (shape, color);
+      break;
+    case 8: //MultiPoint
+    case 18: //MultiPointZ
+    case 28: //MultiPointM
+      buildMultiPointModel (shape, color);
+      break;
+    case 3: //PolyLine
+    case 5: //Polygon
+    case 13: //PolyLineZ
+    case 15: //PolygonZ
+    case 23: //PolyLineM
+    case 25: //PolygonM
+      buildPolygonModels (shape, color);
+      break;
+    case 31: //MultiPatch
+      buildMultiPatchModels (shape, color);
+      break;
+    default:
+      throw "Shape type unknown.";
+    return shape;
+  }
 }
 
--- shapefile_render2d.js
+++ shapefile_render2d.js
@@ -19,14 +19,32 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/**
+ * Represents a color.
+ */
 var Color = Class.create ({
   R: 0,
   G: 1,
   B: 2,
   A: 3,
+  
+  /**
+   * Creates a Color.
+   *
+   * \param r The red channel intensity.
+   * \param g The green channel intensity.
+   * \param b The blue channel intensity.
+   * \param a The alpha channel intensity.
+   */
   initialize: function (r, g, b, a) {
       this.data = [r & 0xFF, g & 0xFF, b & 0xFF, a & 0xFF];
   },
+
+  /**
+   * Converts the color to an RGB string.
+   *
+   * \return The RGB string.
+   */
   to_rgb_string: function () {
       var r = this.data[this.R];
       var g = this.data[this.G];
@@ -34,6 +52,12 @@ var Color = Class.create ({
       return "#" + ((r < 10) ? "0" : "") + r.toString (16) + ((g < 10) ? "0" : "") + g.toString (16) + 
              ((b < 10) ? "0" : "") + b.toString (16);
   },
+
+  /**
+   * Converts the color to an RGBA string.
+   *
+   * \return The RGBA string.
+   */
   to_rgba_string: function () {
       var r = this.data[this.R];
       var g = this.data[this.G];
@@ -45,253 +69,112 @@ var Color = Class.create ({
 });
 
 /**
- * Renders a point in a 2d.
+ * Draws a point onto context.
  *
- * \param shape The shape to render.
- * \param context The context onto which to render.
+ * \param point The point
+ * \param context The onto onto which to draw.
  */
-var PointRenderer2d = Class.create ({
-  initialize: function (shape, context) {
-    context.strokeRect (shape.coords[shape.X], shape.coords[shape.Y], 1, 1);
-  }
-});
+function drawPoint (point, context) {
+  context.strokeRect (point[0], point[1], 1, 1);
+}
 
 /**
- * Renders a PointZ point in a 2d.
+ * Renders a Point onto context.
  *
- * \param shape The shape to render.
- * \param context The context onto which to render.
+ * \param shape The Point shape.
+ * \param context The conext onto which to render.
  */
-var PointZRenderer2d = Class.create (PointRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
+function renderPoint (shape, context) {
+  drawPoint (shape.coords, context);
+}
 
 /**
- * Renders a measured point in a 2d.
+ * Renders a MultiPoint onto context.
  *
- * \param shape The shape to render.
- * \param context The context onto which to render.
+ * \param shape The MultiPoint shape.
+ * \param context The conext onto which to render.
  */
-var PointMRenderer2d = Class.create (PointRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
+function renderMultiPoint (shape, context) {
+  shape.eachVertex (drawPoint, context);
+}
 
 /**
- * Renders a set of multiple points in a 2d.
+ * Renders a Polygon part onto context.
  *
- * \param shape The shape to render.
- * \param context The context onto which to render.
+ * \param shape The Polygon part.
+ * \param context The conext onto which to render.
  */
-var MultiPointRenderer2d = Class.create ({
-  initialize: function (shape, context) {
-    for (var i = 0; i < shape.header[5]; ++i)
-      context.strokeRect (shape.points[i][shape.X], shape.points[i][shape.Y], 1, 1);
+function renderPolygonPart (part, context) {
+  var startPoint = part[0];
+  context.beginPath ();
+  context.moveTo (startPoint[0], startPoint[1]);
+  
+  for (var i = 1; i < part.length; ++i) {
+    var temp = part[i];
+    context.lineTo (temp[0], temp[1]);
   }
-});
+  context.stroke ();
+  context.closePath ();
+}
 
 /**
- * Renders a set of PointZs in a 2d.
+ * Renders a Polygon onto context.
  *
- * \param shape The shape to render.
- * \param context The context onto which to render.
+ * \param shape The Polygon shape.
+ * \param context The conext onto which to render.
  */
-var MultiPointZRenderer2d = Class.create ({
-  initialize: function (shape, context) {
-    $super (shape, context);
-  }
-});
+function renderPolygon (shape, context) {
+  shape.eachPart (renderPolygonPart, context);
+}
 
 /**
- * Renders a set of PointMs in a 2d.
+ * Renders a MultiPatch onto context.
  *
- * \param shape The shape to render.
+ * \param shape The MultiPatch shape.
  * \param context The context onto which to render.
  */
-var MultiPointMRenderer2d = Class.create ({
-  initialize: function (shape, context) {
-    $super (shape, context);
-  }
-});
+function renderMultiPatch (shape, context) {
+  //not yet implemented.
+}
 
 /**
- * Renders a polygon in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonRenderer2d = Class.create ({
-  initialize: function (shape, context) {
-    for (var i = 0; i < shape.header[shape.NUM_PARTS]; ++i) {
-      var startPoint = shape.parts[i][0];
-      context.beginPath ();
-      context.moveTo (startPoint[shape.X], startPoint[shape.Y]);
-      
-      for (var j = 1; j < shape.parts[i].length; ++j) {
-        var temp = shape.parts[i][j];
-        context.lineTo (temp[shape.X], temp[shape.Y]);
-      }
-      context.stroke ();
-      context.closePath ();
-    }
-  }
-});
-
-/**
- * Renders a PolygonZ in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonZRenderer2d = Class.create (PolygonRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
-
-/**
- * Renders a measured polygon in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonMRenderer2d = Class.create (PolygonRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
-
-/**
- * Renders a PolygonZ in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolygonZRenderer2d = Class.create (PolygonRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
-
-/**
- * Renders a PolyLine in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolyLineRenderer2d = Class.create (PolygonRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
-
-/**
- * Renders a PolyLineZ in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolyLineZRenderer2d = Class.create (PolygonRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
-
-/**
- * Renders a PolyLineM in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var PolyLineMRenderer2d = Class.create (PolygonRenderer2d, {
-  initialize: function ($super, shape, context) {
-    $super (shape, context);
-  }
-});
-
-/**
- * Renders a MultiPatch in a 2d.
- *
- * \param shape The shape to render.
- * \param context The context onto which to render.
- */
-var MultiPatchRenderer2d = Class.create ({
-  initialize: function (shape, context) {
-  }
-});
-
-/**
- * Calls the appropriate renderer.
+ * Renders a shape onto a context in the given color.
  *
  * \param shape The shape to render.
  * \param context The context onto which to render.
  *
  * \throw error On unknown shape type.
  */
-function RenderFactory2d (shape, context) {
+function render (shape, context, color) {
+  context.strokeStyle = color.to_rgb_string ();
   switch (shape.header[0]) {
     case 0:
       //NullShape does not need rendering. 
       break;
-    case 1:
-      new PointRenderer2d (shape, context);
+    case 1: //Point
+    case 11: //PointZ
+    case 21: //PointM
+      renderPoint (shape, context);
       break;
-    case 3:
-      new PolyLineRenderer2d (shape, context);
+    case 8: //MultiPoint
+    case 18: //MultiPointZ
+    case 28: //MultiPointM
+      renderMultiPoint (shape, context);
       break;
-    case 5:
-      new PolygonRenderer2d (shape, context);
+    case 5: //Polygon
+    case 15: //PolygonZ
+    case 25: //PolygonM
+    case 3: //PolyLine
+    case 13: //PolyLineZ
+    case 23: //PolyLineM
+      renderPolygon (shape, context);
       break;
-    case 8:
-      new MultiPointRenderer2d (shape, context);
-      break;
-    case 11:
-      new PointZRenderer2d (shape, context);
-      break;
-    case 13:
-      new PolyLineZRenderer2d (shape, context);
-      break;
-    case 15:
-      new PolygonZRenderer2d (shape, context);
-      break;
-    case 18:
-      new MultiPopintZRenderer2d (shape, context);
-      break;
-    case 21:
-      new PointMRenderer2d (shape, context);
-      break;
-    case 23:
-      new PolyLineMRenderer2d (shape, context);
-      break;
-    case 25:
-      new PolygonMRenderer2d (shape, context);
-      break;
-    case 28:
-      new MultiPointMRenderer2d (shape, context);
-      break;
-    case 31:
-      new MultiPatchRenderer2d (shape, context);
+    case 31: //MultiPatch
+      renderMultiPatch (shape, context);
       break;
     default:
       throw "Shape type unknown.";
+    return shape;
   }
-}
-
-/**
- * Renders a shapefile in a two dimensional orthographic projection.
- *
- * \param shapeFile The shapefile.
- * \param context The canvas onto which to draw.
- * \param color, The color to render the shapes.
- *
- * \throw error On trying to render an unknown shape type.
- */
-function render2d (shapeFile, context, color) {
-  context.strokeStyle = color.to_rgb_string ();
-  for (var i = 0; i < shapeFile.header.numShapes; ++i)
-    RenderFactory2d (shapeFile.shapes[i], context);
 }
 
