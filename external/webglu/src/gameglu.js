be7d8a735f8bd12aa12b42398646aaccbb8c299a
Thomas Lahoda <tlahoda@gmail.com>
2011-08-31 15:48:56 -0400

    Partially streamlined webglu. Cleared out most extraneous files.

---
+++ external/webglu/src/gameglu.js
@@ -0,0 +1,232 @@
+/** @ignore Wrapper function to allow multifile or single file organization */
+$W.initGameGLU = function() {
+
+    /** Contains useful input handling functionality
+     * @namespace GameGLU
+     */
+    $G = {
+        offsetX:0,
+        offsetY:0,
+
+        SimulatedWorld:{
+            objects:[]
+        },
+
+        SphereSimulation:{
+
+        },
+
+        /** Convenience object for putting any interaction state data */
+        state:{},
+
+        mouse: [null, 'm1', 'm2'],
+
+        /** Key constants
+         */
+        keys:{
+            m1: 1, m2: 2,
+            backspace :  8, tab  :  9, enter : 13,
+            shift     : 16, ctrl : 17, alt   : 18,
+            pause_break : 19,
+            caps_lock   : 20, esc: 27, space : 32,
+            pgup   : 33, pgdown : 34,
+            end    : 35, home   : 36,
+            insert : 45, del    : 46,
+            left : 37, up : 38, right : 39, down : 40,
+            0 : 48, 1 : 49, 2 : 50, 3 : 51, 4 : 52,
+            5 : 53, 6 : 54, 7 : 55, 8 : 56, 9 : 57,
+            a : 65, b : 66, c : 67, d : 68, e : 69,
+            f : 70, g : 71, h : 72, i : 73, j : 74,
+            k : 75, l : 76, m : 77, n : 78, o : 79,
+            p : 80, q : 81, r : 82, s : 83, t : 84,
+            u : 85, v : 86, w : 87, x : 88, y : 89, z : 90,
+            win : 91,
+            num0 :  96, num1 :  97, num2 :  98, num3 :  99, num4 : 100,
+            num5 : 101, num6 : 102, num7 : 103, num8 : 104, num9 : 105,
+            multiply : 106, plus : 107, minus : 109, dot : 110, divide : 111,
+            f1 : 111,  f2 : 113,  f3 : 114,  f4 : 115,
+            f5 : 116,  f6 : 117,  f7 : 118,  f8 : 119,
+            f9 : 120, f10 : 120, f11 : 122, f12 : 123,
+            num_lock : 144, scroll_lock : 145,
+            comma : 186, equals : 187, dash         : 189, period    : 190,
+            slash : 191, tilde  : 192, left_bracket : 219, backslash : 220,
+            right_bracket : 221, single_quote : 222
+        },
+
+        /** Binds the named key to a particular function
+         *
+         * @param {String} key The name of a key, optionally with + or -
+         * appended to it to specify keyup or keydown respectively, + is assumed
+         * if not supplied. 
+         *
+         * @param {Function} action The function to call when the key event is
+         * triggered.
+         */
+        bind: function(key, action) {
+            $G.event.bind(key, action);
+        },
+
+        /** Enables the control profile system.
+         */
+        useControlProfiles: function() {
+            $W.useControlProfiles();
+        },
+     
+        event:{
+            _keys:[],
+            _actions:[],
+
+            _initialize:function() {
+                var actions = $G.event._actions;
+
+                addEventListener("keydown", function(ev) {
+                    if (actions[ev.keyCode] !== undefined) {
+                        actions[ev.keyCode](ev);
+                        ev.preventDefault();
+                    } 
+                }, false);
+
+                addEventListener("keyup", function(ev) {
+                    if (actions[-ev.keyCode] !== undefined) {
+                        actions[-ev.keyCode](ev);
+                        ev.preventDefault();
+                    } 
+                }, false);
+
+                addEventListener("mousedown", function(ev) {
+                    if (actions[ev.which] !== undefined) {
+                        actions[ev.which](ev.clientX - $G.offsetX, 
+                                          ev.clientY - $G.offsetY, ev);
+                        ev.preventDefault();
+                    }
+                }, false);
+
+                addEventListener("mouseup", function(ev) {
+                    if (actions[-ev.which] !== undefined) {
+                        actions[-ev.which](ev.clientX - $G.offsetX, 
+                                           ev.clientY - $G.offsetY, ev);
+                        ev.preventDefault();
+                    }
+                }, false);
+
+                addEventListener("mousemove", function(ev) {
+                    if (actions["mousemove"] !== undefined) {
+                        actions["mousemove"](ev.clientX -  $G.offsetX, 
+                                             ev.clientY -  $G.offsetY, ev);
+                        ev.preventDefault();
+                    }
+                }, false);
+
+                addEventListener("DOMMouseScroll", function(ev) {
+                    if (actions["mousewheel"] !== undefined) {
+                        actions["mousewheel"](ev.detail ? ev.detail * -1 : ev.wheelData / 40, ev);
+                        ev.preventDefault();
+                    } 
+                }, false);
+            },
+
+            bindOnElement:function(element, eventName, action) {
+                element.addEventListener(eventName, action, false);
+            },
+
+
+            /* Add this key to the list of bound keys so we can only
+             * try to call functions for keys that have been bound.
+             *
+             * Associate the corresponding keycode with the passed function
+             * in the function array.
+             *
+             * '+keyname' or 'keyname' binds to the keydown event
+             * '-keyname' binds to keyup
+             * @param
+             */
+            bind:function(key, action) {
+                var M = $G.mouse
+                var actions = $G.event._actions;
+
+                // Elements
+                if (arguments.length === 3) {
+                    arguments[0].addEventListener(arguments[1], arguments[2]);
+
+                    /*
+                // Mousebuttons XXX Kludge?
+                }else if (key == "+m1" || key == "m1") {
+                    actions['+m1'] = action;
+
+                }else if (key == "+m2" || key == "m2") {
+                    actions['+m2'] = action;
+
+                }else if (key == "-m1") {
+                    actions['-m1'] = action;
+
+                }else if (key == "-m2") {
+                    actions['-m2'] = action;
+                    */
+
+                }else if (key == "mousemove") {
+                    actions['mousemove'] = action;
+
+                }else if (key == "mousewheel") {
+                    actions['mousewheel'] = action;
+                
+                // Keys
+                }else if (key[0] == "+" || key[0] == "-") {
+
+                    if (key[0] == "+") {
+                        key = $G.keys[key.slice(1)];
+                        actions[key] = action;
+
+                    }else if (key[0] == "-") {
+                        key = $G.keys[key.slice(1)];
+                        actions[-key] = action;
+                    }
+
+                // Default to keydown
+                }else {
+                    key = $G.keys[key];
+                    actions[key] = action;
+                }
+            },
+        },
+
+        initialize:function() {
+            console.warn("initialize is deprecated, it is no longer necessary to call");
+            $G.event._initialize();
+            if ($W !== undefined) {
+                $G.offsetX = $W.canvas.offsetLeft;
+                $G.offsetY = $W.canvas.offsetTop;
+            }
+        }
+    }
+
+    $G.event._initialize();
+    if ($W !== undefined) {
+        $G.offsetX = $W.canvas.offsetLeft;
+        $G.offsetY = $W.canvas.offsetTop;
+    }
+
+};
+/*
+ *  Copyright (c) 2009 Benjamin P. DeLillo
+ *  
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ */
---
+++ external/webglu/src/webglu.js
@@ -0,0 +1,395 @@
+/** @author Benjamin DeLillo */
+/*
+     *  Copyright (c) 2009-2010 Benjamin P. DeLillo
+     *  
+     *  Permission is hereby granted, free of charge, to any person
+     *  obtaining a copy of this software and associated documentation
+     *  files (the "Software"), to deal in the Software without
+     *  restriction, including without limitation the rights to use,
+     *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+     *  copies of the Software, and to permit persons to whom the
+     *  Software is furnished to do so, subject to the following
+     *  conditions:
+     *  
+     *  The above copyright notice and this permission notice shall be
+     *  included in all copies or substantial portions of the Software.
+     *  
+     *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+     *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+     *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+     *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+     *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+     *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+     *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+     *  OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/** 
+ * The primary WebGLU namespace.
+ * Holds drawing related data and has useful functions and objects for
+ * working with WebGL.
+ * @namespace WebGLU
+ */
+$W = {
+
+    createdObjectCount:0,
+    
+    /** Various paths used by WebGLU. 
+     * @property {String} libsrc Path to *GLU libraries themselved 
+     * @property {String} shaders Where shaders are stored
+     * @property {String} external Where external libraries, like Sylvester, are stored
+     * @property {String} textures
+     * @property {String} materials
+     * @property {String} sylvester Which Sylvester lib to load, Sylvester.src.js or Sylvester.js
+     */
+    paths:{
+        libsrc : "../external/webglu/src/",
+        shaders : "../external/webglu/shaders/",
+        external : "../external/webglu/external/",
+        textures : "../external/webglu/textures/",
+        materials : "../external/webglu/materials/",
+        sylvester : "Sylvester.src.js"
+    },
+
+    /** The raw WebGL object for low level work.  */
+    GL : null,
+
+
+    /** Renderable objects */
+    renderables:[],
+    pickables  :[],
+    objects    :[],
+
+    /** Shaders */
+    shaders  : [],
+
+    /** Shader Programs */
+    programs : [],
+
+    /** Textures */
+    textures : [],
+
+    /** Materials */
+    materials: [],
+
+    /** Model-View transform matrix */
+    modelview:null,
+
+    /** Projection transform matrix */
+    projection:null,
+
+    /** Viewport camera
+     * @type Camera
+     */
+    camera:null,
+
+    /** Quick access to FPS */
+    FPS : 0,
+
+    /** Provides frame timing */
+    timer:null,
+
+    /** Canvas object being rendered to */
+    canvas:null,
+
+    /** Clears the entire WebGLU system to pre-init state */
+    reset: function() {
+        $W.GL = null;
+        $W.modelview = null;
+        $W.projection = null;
+        $W.camera = null;
+        $W.fpsTracker = null;
+        $W.FPS = 0;
+        $W.timer = null;
+        $W.canvas = null;
+        $W.objects = [];
+        $W.renderables = [];
+        $W.pickables = [];
+        $W.shaders = [];
+        $W.programs = [];
+        $W.textures = [];
+        $W.materials = [];
+    },
+
+    /** Override this to provide custom drawing functionality
+     */
+    draw: function(){},
+
+    /** Override this to provide custom updating functionality
+     */
+    update: function(){},
+
+    _draw: function $W_draw() {
+        $W.util.defaultDraw();
+        $W.draw();
+    },
+    _update: function $W_draw() {
+        $W.util.defaultUpdate();
+        $W.update();
+    },
+
+    /** Begin rendering the scene 
+     * @param {Number} [framelimit] Optional framerate cap
+     */
+    start: function $W_start(framelimit) {
+
+        // use mozRequestAnimationFrame if available
+        if (typeof(window.mozRequestAnimationFrame) != 'undefined') {
+            var redraw = function $W_mozRedraw() {
+                $W._update();
+                $W._draw();
+                window.mozRequestAnimationFrame(redraw);
+            };
+            window.mozRequestAnimationFrame(redraw);
+
+        // fallback to setinterval
+        }else {
+            if (typeof(framelimit) === 'undefined') {
+                framelimit = 10;
+            }
+            setInterval(function $W_redrawFn() {
+                    $W._update();
+                    $W._draw();
+                    },framelimit);
+        }
+    },
+
+    /** Provides utility functions and classes for working with WebGLU
+     * @namespace Util
+     */
+    util: {
+        /** Load the file at the given path as text.
+         * @param {String} path The path to the file.
+         * @return {String} Data in the file as text.
+         */
+        loadFileAsText:function(path) {
+            $W.info("Loading file `" + path + "`");
+            var xhr = null;
+            xhr = new XMLHttpRequest();
+
+            if (!xhr) { 
+                return null; 
+            }
+
+            xhr.overrideMimeType("text/plain");
+
+            // Deal with firefox security for file:// urls
+            try {
+                var nsPM = null;
+                if (typeof(netscape) !== 'undefined' && 
+                    typeof(netscape.security) !== 'undefined' && 
+                    typeof(netscape.security.PrivilegeManager) !== 'undefined') {
+                    nsPM = netscape.security.PrivilegeManager;
+                }
+                if (document.location.href.match(/^file:\/\//)) {
+                    if (nsPM !== null) {
+                        nsPM.enablePrivilege("UniversalBrowserRead");
+                    }
+                }
+            }catch (e) {
+                console.error(e);
+                throw "\tBrowser security may be restrcting access to local files";
+            }
+
+            try {
+
+                xhr.open("GET", path, false);
+
+                // Ignore cache if the file is served from localhost or is at a
+                // file:// url as it's safe to assume that's a developer.
+                var url = window.location.href;
+                if (    url.match(/^http:\/\/localhost/) !== null ||
+                        url.match(/^http:\/\/127\.0\.0\.1/) !== null ||
+                        url.match(/^file:/) !== null) {
+
+                    xhr.setRequestHeader('Pragma', 'Cache-Control: no-cache');
+                }
+
+                xhr.send(null);
+
+            }catch (e) { 
+                throw e; 
+            }
+
+
+            if (xhr.status !== 200) {
+                console.error("\tCompleted with status: " + xhr.status);
+                return "File load error: " + xhr.status;
+            }else {
+                $W.debug("\tCompleted with status: " + xhr.status);
+                return xhr.responseText;
+            }
+
+        },
+
+        include: function(path) {
+            $W.info('Including ' + path);
+            var script = $W.util.loadFileAsText(path);
+            $W.log(path);
+            window.eval(script);
+        }
+    },
+
+
+    /** 
+     * Initialize the WebGLU system, optionally initizlizing the WebGL
+     * subsystem.
+     * canvas - the DOM node or id string of a canvas element to init WebGL,
+     *          defaults to using the DOM element with ID 'canvas'.
+     *          Pass `false` to skip init of WebGL subsystem.
+     */
+    initialize:function $W_initialize(canvas) {
+        $W.initLogging();
+        $W.log("Initializing WebGLU");
+
+        var initModule = function(module) {
+            // If this is not the single file version, load each module
+            if (typeof($W['init' + module]) === 'undefined') {
+                $W.util.include($W.paths.libsrc + module + '.js');
+            }
+
+            // In either case call the appropriate init function
+            $W['init' + module]();
+        };
+
+        // Init Util module early so we can bail out fast.
+        initModule('Util');
+        $W.util.setCanvas(canvas);
+        if (!$W.util.hasWebGLTypes()) {
+           $W.util.displayWebGLFailure(); 
+           return;
+        }
+
+        var modules = ['Constants', 'DefaultUniformActions', 'GLSL',
+            'GLU', 'Animation', 'Object', 'Texture', 'Framebuffer', 'Material',
+            'Renderer' ];
+
+        for (var i = 0; i < modules.length; i++) {
+            initModule(modules[i]);
+        }
+
+        // create the matrix stacks we'll be using to store transformations
+        $W.modelview  = new $W.GLU.MatrixStack();
+        $W.projection = new $W.GLU.MatrixStack();
+
+        $W.camera     = new $W.Camera();
+        $W.timer      = new $W.Timer();
+
+        if (canvas === false) {
+            console.log("WebGL init skipped");
+        }else {
+            if ($W.util.initWebGL()) {
+                $W.camera.aspectRatio = $W.canvas.width / $W.canvas.height;
+                $W.util.loadDefaultAssets();
+                return true;
+            }else {
+                return false;
+            }
+        }
+    },
+
+    /** Ensure that we can log, or at least not error if we try to */
+
+    /** Use debugging context.
+     * Must be called <i>after</i> initialize().
+     */
+    useWebGLDebug: function() {
+        $W.util.include($W.paths.external + 'webgl-debug.js');
+        $W.GL = WebGLDebugUtils.makeDebugContext($W.GL);
+    },
+
+    useControlProfiles: function() {
+        if (typeof($W['initControlProfiles']) == 'undefined') {
+            $W.util.include($W.paths.libsrc + 'ControlProfiles.js');
+        }
+        $W.initControlProfiles();
+    },
+
+    useCrazyGLU: function() {
+        if (typeof($W['initCrazyGLU']) == 'undefined') {
+            $W.util.include($W.paths.libsrc + 'crazyglu.js');
+        }
+        $W.initCrazyGLU();
+    },
+
+    useGameGLU: function() {
+        if (typeof($W['initGameGLU']) == 'undefined') {
+            $W.util.include($W.paths.libsrc + 'gameglu.js');
+        }
+        $W.initGameGLU();
+    },
+};
+
+$W.LL = {
+DEBUG :0,
+       INFO  :1,
+       NOTE  :2,
+       WARN  :3
+};
+$W.loglevel = $W.LL.NOTE;
+$W.initLogging = function() {
+        if (window.console === undefined) {
+            console = {};
+        } // Dummy object
+
+        if (console.log === undefined) {
+            console.log = function(){};
+        }else {
+            $W.logPrefix = '\t';
+            $W.logIndentAmount = 0;
+            $W.getLogPrefix = function() {
+                var prefix = '';
+                for (var i = 0; i < $W.logIndentAmount; i++) {
+                    prefix += $W.logPrefix;
+                }
+                return prefix;
+            };
+            $W.indentLog = function(){$W.logIndentAmount++;};
+            $W.dedentLog = function(){$W.logIndentAmount--;};
+                    
+            $W.log = function(message, level) {
+                if (typeof(level) === 'undefined') {level = $W.LL.NOTE;}
+
+                if (level >= $W.loglevel) {
+                    if (typeof(message) === 'string') {
+                        console.log($W.getLogPrefix() + message);
+                    }else {
+                        console.dir(message);
+                    }
+                }
+            };
+            $W.info = function(message) {
+                $W.log(message, $W.LL.INFO);
+            };
+            $W.debug = function(message) {
+                $W.log(message, $W.LL.DEBUG);
+            };
+            $W.warn = function(message) {  
+                if ($W.LL.WARN >= $W.loglevel) {
+                    console.warn($W.getLogPrefix() + message);
+                }
+            };
+        }
+
+        // If console.log exists, but one or more of the others do not,
+        // use console.log in those cases.
+        if (console.dir === undefined) {
+            console.dir           = console.log;
+        }
+        if (console.warn === undefined) {
+            console.warn           = console.log;
+        }
+        if (console.error === undefined) {
+            console.error          = console.log;
+        }
+        if (console.group === undefined) {
+            console.group          = console.log;
+        }
+        if (console.groupCollapsed === undefined) {
+            console.groupCollapsed = console.group;
+        }
+        if (console.groupEnd === undefined) {
+            console.groupEnd       = console.log;
+        }
+    };
+$W.initLogging();
