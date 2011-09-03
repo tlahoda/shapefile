f391fc21e14ba67d7b01b6e663fda91918d515c4
Thomas Lahoda <tlahoda@gmail.com>
2011-09-02 20:23:04 -0400

    Moved makeVertexColors inline since it is only used one place. Renamed buildModel to makeModel.

--- model_builders.js
+++ model_builders.js
@@ -32,24 +32,7 @@ function set (ele, value) {
 }
 
 /**
- * Makes an array of colors for ian array of vertices.
- *
- * \param vertices The array of vertices for whic hto generate colors.
- * \param color The color to make the vertices.
- *
- * \return The array of colors.
- */
-function makeVertexColors (vertices, colors) {
-  var newColors = new Array (vertices.length);
-  for (var i = 0, j = 0; i < vertices.length; ++i) {
-    newColors[i] = colors[j];
-    if (++j >= colors.length) j = 0;
-  }
-  return newColors;
-}
-
-/**
- * Builds a model.
+ * Makes a model.
  *
  * \param modelType The type of the model.
  * \param vertices The model vertices.
@@ -57,11 +40,11 @@ function makeVertexColors (vertices, colors) {
  *
  * \return The model.
  */
-function buildModel (modelType, vertices, colors) {
+function makeModel (modelType, vertices, color) {
   var model = new $W.Object (modelType);
   model.vertexCount = vertices.length;
   model.fillArray ("vertex", vertices);
-  model.fillArray ("color", makeVertexColors (vertices, colors));
+  model.fillArray ("color", new Array (vertices.length).apply (set, color));
   return model;
 }
 
@@ -124,9 +107,9 @@ function addMultiPatch (shape, vertices) {
  * Builds the models for a shape.
  *
  * \param shape The shape.
- * \param colors The colors to render the shapes.
+ * \param color The color to render the shape.
  */
-function buildModels (shape, colors) {
+function buildModels (shape, color) {
   var vertices = new Array ();
   var modelType = $W.GL.LINES;
   switch (shape.header[0]) {
@@ -137,11 +120,13 @@ function buildModels (shape, colors) {
     case 11: //PointZ
     case 21: //PointM
       addPoint (shape, vertices);
+      modelType = $W.GL.POINTS;
       break;
     case 8: //MultiPoint
     case 18: //MultiPointZ
     case 28: //MultiPointM
       addMultiPoint (shape, vertices);
+      modelType = $W.GL.POINTS;
       break;
     case 3: //PolyLine
     case 5: //Polygon
@@ -157,6 +142,6 @@ function buildModels (shape, colors) {
     default:
       throw "Shape type unknown.";
   }
-  shape.model = buildModel (modelType, vertices, colors);
+  shape.model = makeModel (modelType, vertices, color);
 }
 
